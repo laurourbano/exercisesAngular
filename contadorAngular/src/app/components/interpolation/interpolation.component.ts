5a598e18da072e6186424853a23ad254ce679160
laurourbano <lauro@outlook.com.br>
2023-10-21 22:50:12 -0300

    diretiva ngFor

--- contadorAngular/src/app/components/interpolation/interpolation.component.ts
+++ contadorAngular/src/app/components/interpolation/interpolation.component.ts
@@ -9,4 +9,11 @@ export class InterpolationComponent {
   imgURL = 'https://img.freepik.com/fotos-gratis/conceito-de-rpa-com-tela-de-toque-de-mao-embacada_23-2149311914.jpg?w=900&t=st=1697934341~exp=1697934941~hmac=d60302fe2844cd9a10ed94bfefff7c30d59807ae4281aa7f77b6a31cca005fcf';
   url = 'https://devmedia.com.br';
 
+  valorAtual = true;
+
+  livros: string[] = [ 'Angular', 'Java', 'Spring' ];
+
+  mostrar() {
+    this.valorAtual = !this.valorAtual;
+  }
 }
